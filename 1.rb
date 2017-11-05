a8259131b4f1bd822a3369e6c9f7e7f0014bddb5
府川和樹 <kazukifukawa1012@gmail.com>
2017-11-05 23:58:05 +0900

    answer add

--- 1.rb
+++ 1.rb
@@ -8,6 +8,19 @@ end
 
 puts "何か値を入力してください………"
 
-input = gets.chomp
+input1 = gets.chomp
 
-puts_something(input)
+puts_something(input1)
+
+
+#-------      answer   ------------------
+
+# def puts_something(something)
+#   puts "#{something}!"
+# end
+
+# puts "何か値を入力してください・・・"
+
+# string = gets.chomp
+
+# puts_something(string)
