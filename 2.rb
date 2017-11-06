cea305f2f377bac6e2ad8de26e6b010e3b38c3e7
府川和樹 <kazukifukawa1012@gmail.com>
2017-11-06 23:03:47 +0900

    add file

--- 2.rb
+++ 2.rb
@@ -14,3 +14,19 @@ input2 = gets.to_i
 
 
 test(input1,input2)
+
+
+
+# def multiplication(num1, num2)
+#   puts "#{num1}と#{num2}をかけた答えは#{num1 * num2}です！"
+# end
+
+# puts "最初の数字を入力してください"
+
+# num1 = gets.to_i
+
+# puts "2番目の数字を入力してください"
+
+# num2 = gets.to_i
+
+# multiplication(num1, num2)
