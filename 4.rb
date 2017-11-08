c0aec16ff2e3a5fb22e34c6718339f8f53c9ae09
府川和樹 <kazukifukawa1012@gmail.com>
2017-11-08 17:09:22 +0900

    add file

---
+++ 4.rb
@@ -0,0 +1,15 @@
+
+def function
+
+	puts '"以下から一つ選んでください'
+	puts"・title"
+	puts"・genre"
+	puts'・year"'
+
+end
+
+function
+
+movie = {"title" => "ハリーポッター", "genre" => "ファンタジー", "year" => "2001年"}
+
+puts movie{"title"}
