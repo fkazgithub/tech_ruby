82b0c600e702958f36a5a8296f815020b6be13a9
府川和樹 <kazukifukawa1012@gmail.com>
2017-11-09 22:49:46 +0900

    add

--- 4.rb
+++ 4.rb
@@ -12,4 +12,23 @@ function
 
 movie = {"title" => "ハリーポッター", "genre" => "ファンタジー", "year" => "2001年"}
 
-puts movie{"title"}
+input = gets.chomp
+
+puts "#「#{input}」を入力"
+
+puts movie[input]
+
+# def movie_info(movie, data)
+#   puts movie[data]
+# end
+
+# movie = {"title" => "ハリーポッター", "genre" => "ファンタジー", "year" => "2001年"}
+
+# puts "以下から一つを選んで入力してください。
+#   ・title
+#   ・genre
+#   ・year"
+
+# info = gets.chomp
+
+# movie_info(movie, info)
