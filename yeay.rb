fa904da27580b7c183bd45b12fe51d4971492c29
府川和樹 <kazukifukawa1012@gmail.com>
2017-11-11 23:54:12 +0900

    add file

---
+++ yeay.rb
@@ -0,0 +1,13 @@
+def get_days(year, month)
+    # ここに処理を書き加えてください
+
+
+  end
+
+  puts "年を入力してください："
+  year = gets.to_i
+  puts "月を入力してください："
+  month = gets.to_i
+
+  days = get_days(year, month)
+  puts "#{year}年#{month}月は#{days}日間あります"
